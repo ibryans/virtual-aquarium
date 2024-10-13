bc096d8f26bf55b95b2ea0434dc0a9b0f12f9883
Bryan Oliveira <bsoliveira@proton.me>
2024-10-13 12:02:21 -0300

    feat: Finaliza a movimentação dos peixes

--- src/components/FishItem.vue
+++ src/components/FishItem.vue
@@ -2,7 +2,7 @@
     import Fish from '@/models/Fish';
     import dead from '@/assets/images/dead.png'
     import { store } from '@/store';
-    import { onMounted, ref } from 'vue';
+    import { onMounted, onUnmounted, ref } from 'vue';
     
     defineProps({
         fish: {
@@ -11,61 +11,79 @@
         }
     })
 
-    const xPosition = ref(0)
-    const yPosition = ref(0)
+    // Pega a div do aquario
+    const aquarium = ref(document.getElementById('aquarium'));
 
+    // Se ta indo pra direita ou esquerda (para inverter a imagem do peixe)
+    const goingBack = ref(false);
+
+    // Define uma posição aleatória de início
+    const xPosition = ref(
+      Math.random() * ((aquarium.value?.clientWidth || 500) - 200)
+    )
+    const yPosition = ref(
+      Math.random() * ((aquarium.value?.clientHeight || 500) - 200)
+    )
+
+    // Faz o peixe se mover para uma posição aleatória (mudando o top e left)
     const move = () => {
-      xPosition.value = Math.random() * (window.innerWidth - 500)
-      yPosition.value = Math.random() * (window.innerHeight - 500)
+      if (aquarium.value) {
+        const x = Math.random() * (aquarium.value.clientWidth - 176)
+        
+        goingBack.value = x < xPosition.value;
+
+        xPosition.value = x;
+        yPosition.value = Math.random() * (aquarium.value.clientHeight - 176)
+      }
     }
 
     onMounted(() => {
       move();
-      setInterval(() => move(), 2000)
+      const interval = setInterval(() => move(), 8000) // Se move a cada 8s
+      onUnmounted(() => clearInterval(interval))
     })
-
-
 </script>
 
 <template>
-    <div 
-      class="p-5 flex flex-col w-44 relative swim"
+  <div class="relative">
+    <div
+      class="absolute p-5 flex flex-col w-44" 
       :style="{
         left: `${xPosition}px`,
         top: `${yPosition}px`,
         transition: 'all 8s linear'
       }"
     >
-        
-        <!-- Me alimente -->
-        <button
-            @click="() => store.feedFish(fish)"
-            v-if="fish.hungerLevel >= 75 && fish.hungerLevel < 100"
-            class="absolute top-0 right-0 rounded-full bg-white text-center p-2 hover:text-red-500 transition">
-                Comida!
-        </button>
 
-        <img :src="fish.hungerLevel < 100 ? fish.image : dead"/>
+      <button
+          @click="() => store.feedFish(fish)"
+          v-if="fish.hungerLevel >= 75 && fish.hungerLevel < 100"
+          class="absolute z-20 top-0 right-10 -mt-6 rounded-full bg-white text-center m-2 p-2 hover:text-red-500 transition">
+            Comida!
+      </button>
 
-        <span class="rounded bg-black opacity-70 text-white text-center p-2">
-          {{ fish.name }}
-        </span>
+      <img 
+        :src="fish.hungerLevel < 100 ? fish.image : dead"
+        :style="{
+          transform: `scaleX(${goingBack ? -1 : 1})`,
+          transition: 'all 0.1s linear'
+        }"  
+      />
 
-        <!-- Barra de fome -->
-        <div class="relative w-full h-1 bg-black">
-            <div 
-                :style="{ width: `${fish.hungerLevel}%` }"
-                class="absolute h-full"
-                :class="fish.hungerLevel < 25 
-                    ? 'bg-green-500' : fish.hungerLevel < 50
-                    ? 'bg-yellow-500' : fish.hungerLevel < 75
-                    ? 'bg-orange-500' : 'bg-red-500' ">
-            </div>
-        </div>
+      <span class="rounded bg-black opacity-70 text-white text-center p-2">
+        {{ fish.name }}
+      </span>
+
+      <div class="relative w-full h-1 bg-black">
+          <div 
+              :style="{ width: `${fish.hungerLevel}%` }"
+              class="absolute h-full"
+              :class="fish.hungerLevel < 25 
+                  ? 'bg-green-500' : fish.hungerLevel < 50
+                  ? 'bg-yellow-500' : fish.hungerLevel < 75
+                  ? 'bg-orange-500' : 'bg-red-500' ">
+          </div>
+      </div>
     </div>
+  </div>
 </template>
-
-<style lang="css" scoped>
-
-
-</style>
